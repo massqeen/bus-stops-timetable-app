f4ab0721d8715ecce4f504225aeadec707226b07
Arthur Pliuscchevskyi <ap@hilfy.net>
2024-08-10 14:20:18 +0300

    Add new components for sorting and line's stop list display
    - Created SortIcon component to display sorting state
    - Created BusLineStopList component to display bus lines and stops with sorting functionality
    - Updated color variables

---
+++ src/components/BusLineStopList.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { defineProps, ref, watch } from 'vue'
+import BusStopItem from '@/components/BusStopItem.vue'
+import SortIcon from '@/components/icons/SortIcon.vue'
+import { BusStop } from '@/store'
+
+const props = defineProps<{ stops: BusStop[], selectedLineNumber: number }>()
+
+const sortedBusStops = ref([...props.stops])
+const isAscending = ref(true)
+
+const sortStopsByOrder = () => {
+  if (isAscending.value) {
+    sortedBusStops.value.sort((a, b) => b.order - a.order)
+  } else {
+    sortedBusStops.value.sort((a, b) => a.order - b.order)
+  }
+  isAscending.value = !isAscending.value
+}
+
+watch(() => props.stops, (newStops) => {
+  sortedBusStops.value = [...newStops].sort((a, b) => a.order - b.order)
+}, { immediate: true })
+</script>
+
+<template>
+  <div class="line-stops">
+    <h2 class="title">Bus Line: {{ props.selectedLineNumber }}</h2>
+    <div class="sorting-wrapper">
+      <p class="sorting">Bus Stops</p>
+      <button class="sorting-button" @click="sortStopsByOrder">
+        <SortIcon :isSortedAsc="isAscending" />
+      </button>
+    </div>
+    <div class="stop-list">
+      <BusStopItem v-for="stop in sortedBusStops" :key="stop.order" :stop="stop" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/styles/variables';
+@import '@/styles/mixins';
+
+.line-stops {
+  height: 44.4rem;
+  padding: 2.4rem 0;
+  background-color: $primary-white;
+  border-radius: 4px;
+  overflow-y: scroll;
+  -ms-overflow-style: none;  /* Internet Explorer 10+ */
+  scrollbar-width: none;  /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none;  /* Safari and Chrome */
+  }
+
+  .title {
+    padding: 0 2.4rem;
+    @include font(1.71, $font-size-sm, 600);
+    color: $text-color;
+  }
+
+  .sorting-wrapper {
+    display: flex;
+    align-items: center;
+    padding: 2.4rem;
+    border-bottom: 1px solid $rectangle-border-dark;
+  }
+
+  .sorting {
+    @include font(1.71, $font-size-sm-1, 600);
+    color: $tab-text-color;
+  }
+
+  .stop-list {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .sorting-button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    width: 16px;
+    height: 16px;
+  }
+}
+</style>
---
+++ src/components/icons/SortIcon.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  isSortedAsc: boolean
+}>()
+</script>
+
+<template>
+  <svg width="6" height="8" viewBox="0 0 6 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M3 0L5.5 3H0.5L3 0Z"
+        :fill="props.isSortedAsc ? '#000' : '#9A9DA4'"
+    />
+    <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M3 8L5.5 5H0.5L3 8Z"
+        :fill="!props.isSortedAsc ? '#000' : '#9A9DA4'"
+    />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  cursor: pointer;
+}
+</style>
