656aefb2d0eb43d1697f5e4da443925019fcab58
Arthur Pliuscchevskyi <ap@hilfy.net>
2024-08-10 17:56:30 +0300

    Update BusStops to get unique stops and render them in ascending order - Added logic to get stops from store, remove duplicates and render them in ascending order - Added logic to sort stops by name - Updated styles

--- src/views/BusStops.vue
+++ src/views/BusStops.vue
@@ -1,30 +1,46 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { useStore, BusStop } from '@/store'
-import BusStopItem from '@/components/BusStopItem.vue'
+import SortableBusStopList from "@/components/SortableBusStopList.vue";
 
 const store = useStore()
 const busStops = ref<BusStop[]>([])
+const sortedBusStops = ref([...busStops.value])
+const isAscending = ref(true)
+
+const sortStopsByName = () => {
+  if (isAscending.value) {
+    sortedBusStops.value.sort((a, b) => b.stop.localeCompare(a.stop))
+  } else {
+    sortedBusStops.value.sort((a, b) => a.stop.localeCompare(b.stop))
+  }
+  isAscending.value = !isAscending.value
+}
+
+watch(busStops, (newStops) => {
+  if (!newStops) return
+  sortedBusStops.value = [...newStops].sort((a, b) => a.stop.localeCompare(b.stop))
+}, { immediate: true })
 
 onMounted(async () => {
   await store.dispatch('fetchBusStops')
-  busStops.value = store.state.busStops
+  const uniqueStops = new Map()
+  store.state.busStops.forEach(stop => {
+    if (!uniqueStops.has(stop.stop)) {
+      uniqueStops.set(stop.stop, stop)
+    }
+  })
+  busStops.value = Array.from(uniqueStops.values())
 })
-
-const selectStop = (stop: BusStop) => {
-  // handle stop selection
-  console.log('stop', stop)
-}
 </script>
 
 <template>
   <div class="stops">
-    <div class="stop-selection">
-      <h2 class="title">Select Bus Stop</h2>
-      <div class="stops-list">
-        <BusStopItem v-for="stop in busStops" :key="stop.order" :stop="stop" @stopSelected="selectStop" />
-      </div>
-    </div>
+    <SortableBusStopList :is-selectable="false"
+                         :stops="sortedBusStops"
+                         :is-ascending="isAscending"
+                         @sort-stops="sortStopsByName"
+    />
   </div>
 </template>
 
@@ -32,16 +48,8 @@ const selectStop = (stop: BusStop) => {
 @import '@/styles/variables';
 
 .stops {
-  .stop-selection {
-    .title {
-      font-size: $font-size-lg;
-    }
-
-    .stops-list {
-      display: flex;
-      flex-direction: column;
-      gap: 1rem;
-    }
-  }
+  background-color: $primary-white;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
 }
 </style>
