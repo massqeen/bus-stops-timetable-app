f184238beab1f5f5f3728e28c93510e813a51cc7
Arthur Pliuscchevskyi <ap@hilfy.net>
2024-08-10 10:35:11 +0300

    Create BusLines and Stops views
    - Added BusLines view with navigation and placeholder for bus lines.
    - Added Stops view with navigation and placeholder for bus stops.
    - Styled views using SCSS and imported global variables.

---
+++ src/views/BusLines.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bus-lines">
+    <h1>Timetable</h1>
+    <nav>
+      <router-link to="/" class="nav-link">Bus Lines</router-link>
+      <router-link to="/stops" class="nav-link">Stops</router-link>
+    </nav>
+    <div class="placeholder">
+      Please select the bus line first
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/styles/variables';
+
+.bus-lines {
+  padding: $padding;
+
+  h1 {
+    font-size: $font-size-lg;
+    margin-bottom: $padding;
+  }
+
+  nav {
+    margin-bottom: $padding;
+
+    .nav-link {
+      margin-right: $margin-right;
+      text-decoration: none;
+      color: $primary-color;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .placeholder {
+    font-size: $font-size-md;
+    color: $secondary-color;
+  }
+}
+</style>
---
+++ src/views/Stops.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="stops">
+    <h1>Timetable</h1>
+    <nav>
+      <router-link to="/" class="nav-link">Bus Lines</router-link>
+      <router-link to="/stops" class="nav-link">Stops</router-link>
+    </nav>
+    <div class="placeholder">
+      Please select the bus stop first
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/styles/variables';
+
+.stops {
+  padding: $padding;
+
+  h1 {
+    font-size: $font-size-lg;
+    margin-bottom: $padding;
+  }
+
+  nav {
+    margin-bottom: $padding;
+
+    .nav-link {
+      margin-right: $margin-right;
+      text-decoration: none;
+      color: $primary-color;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .placeholder {
+    font-size: $font-size-md;
+    color: $secondary-color;
+  }
+}
+</style>
