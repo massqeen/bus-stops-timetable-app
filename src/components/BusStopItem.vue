4654b128df013f9847ccaa59dea3bfc492a459c9
Arthur Pliuscchevskyi <ap@hilfy.net>
2024-08-10 14:53:12 +0300

    Update styles of BusStopItem.vue

--- src/components/BusStopItem.vue
+++ src/components/BusStopItem.vue
@@ -16,13 +16,21 @@ const selectStop = () => {
 
 <style scoped lang="scss">
 @import '@/styles/variables';
+@import '@/styles/mixins';
 
 .bus-stop-item {
+  padding: 2rem 2.4rem;
   cursor: pointer;
   transition: background-color 0.3s;
+  @include font(1.33, $font-size-sm-1, 400);
+  color: $tab-text-color;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid $rectangle-border-light;
+  }
 
   &:hover {
-    background-color: lighten($primary-color, 40%);
+    background-color: $fill-light-grey;
   }
 }
 </style>
