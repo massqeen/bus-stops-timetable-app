1d114b899fafc77ec575101663d53afa3b8b2d20
Arthur Pliuscchevskyi <ap@hilfy.net>
2024-08-11 15:45:33 +0300

    Add test config file

---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
+
+export default defineConfig({
+    plugins: [vue(), vueJsx()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+    },
+})
